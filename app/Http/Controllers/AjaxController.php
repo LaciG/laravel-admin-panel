fe1fa8540d8d87ca9c5e8c2948a65b538e62dec9
László Gábor <lacigabi@windowslive.com>
2018-06-13 16:29:33 +0200

    Calculator Calculating

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -12,7 +12,7 @@ class AjaxController extends Controller
 
         $data = $request->toArray();
 
-        $response = array(
+        $calculatorData = array(
             'status' => 'success',
             'workers' => $data['workers'],
             'clients' => $data['clients'],
@@ -25,6 +25,51 @@ class AjaxController extends Controller
             'incident' => $data['incident'],
             'package' => $data['package'],
         );
+
+        $gdpr_data = DB::table('gdpr_settings')->where('workers', '=', $calculatorData['workers'])->where('clients', '=', $calculatorData['clients'])->first();
+        
+        $package_name = $calculatorData['package'];
+        $standard_package = $gdpr_data->$package_name* $gdpr_data->daily_price;
+
+        if($package_name == "DPO") {
+            $response = array(
+                'packdata' => array(
+                    'workers' => $calculatorData['workers'],
+                    'clients' => $calculatorData['clients'],
+                    'industry' => $calculatorData['industry'],
+                    'process' => $calculatorData['process'],
+                    'law' => $calculatorData['law'],
+                    'hr' => $calculatorData['hr'],
+                    'payroll' => $calculatorData['payroll'],
+                    'it' => $calculatorData['it'],
+                    'incident' => $calculatorData['incident'],
+                ),
+                'nodatasecure' => array(
+                    'packname' => $package_name,
+                    'standardpackage' => $standard_package,
+                    'followprice' => $gdpr_data->follow_price
+                )
+            );
+        } else {
+            $response = array(
+                'packdata' => array(
+                    'workers' => $calculatorData['workers'],
+                    'clients' => $calculatorData['clients'],
+                    'industry' => $calculatorData['industry'],
+                    'process' => $calculatorData['process'],
+                    'law' => $calculatorData['law'],
+                    'hr' => $calculatorData['hr'],
+                    'payroll' => $calculatorData['payroll'],
+                    'it' => $calculatorData['it'],
+                    'incident' => $calculatorData['incident'],
+                ),
+                'nodatasecure' => array(
+                    'packname' => $package_name,
+                    'standardpackage' => $standard_package
+                )
+            );
+        }
+
         return response()->json($response); 
     }
 }
--- public/js/ajax.js
+++ public/js/ajax.js
@@ -72,12 +72,58 @@ $(document).ready(function (){
                 dataType: 'json',
                 data: JSON.stringify(calculatorData),
 
-                success: function (response) {
-                    console.log(response);
+                success: function (data) {
+                    console.log(data);
+                    var obj = data;
 
+                     //Calculator Datas
+                     var workers = obj["packdata"]["workers"];
+                     var clients = obj["packdata"]["clients"];
+                     var industry = obj["packdata"]["industry"];
+                     var process = obj["packdata"]["process"];
+                     var law = obj["packdata"]["law"];
+                     var hr = obj["packdata"]["hr"];
+                     var payroll = obj["packdata"]["payroll"];
+                     var it = obj["packdata"]["it"];
+                     var incident = obj["packdata"]["incident"];
+
+                     var package_name = obj["nodatasecure"]["packname"];
+                    var standard_price = obj["nodatasecure"]["standardpackage"];
+                    var follow_price = obj["nodatasecure"]["followprice"];
+
+
+                    $("#package-name").html(package_name);
+                    $("#pack_name").html(package_name);
+
+                    $("#standard-price").html(standard_price);
+                    $("#pack_price").html(standard_price);
+
+                    $("#follow_price").html(follow_price);
+                    $("#fol_price").html(follow_price);
+
+                    console.log("Követés: " + follow_price);
+                    if(follow_price == undefined){
+                        //console.log("Undefined");
+                        $('.followprice-js').hide();
+                        $("#fol_price").text('');
+                    } else {
+                        //console.log("Defined");
+                        $('.followprice-js').show();
+                    }
+
+                    //Formba mennek
+                    $("#workers_data").html(workers);
+                    $("#clients_data").html(clients);
+                    $("#industry_data").html(industry);
+                    $("#process_data").html(process);
+                    $("#law_data").html(law);
+                    $("#hr_data").html(hr);
+                    $("#payroll_data").html(payroll);
+                    $("#it_data").html(it);
+                    $("#incident_data").html(incident);
 
                 },
-                error: function (response) {
+                error: function (data) {
                     console.log("error");
                 }
                 }); 
